a8812386f2866cfcb3f012b1c746578685e34c4f
Rahul-Agarwal-03 <78263007+Rahul-Agarwal-03@users.noreply.github.com>
2022-03-16 11:52:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicTabs from './tab';
+import CustomPaginationActionsTable from './technical';
+import CustomPaginationActionsTabledDesign from './design';
+import CustomPaginationActionsTableManagement from './management';
+function App() {
+  return (
+    <BasicTabs />
+  );
+}
+
+export default App;
